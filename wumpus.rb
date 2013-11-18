96d1983abbbd7810201f34dbae664c58c387eb66
Gregory Brown <gregory.t.brown@gmail.com>
2013-11-18 10:53:08 -0500

    A simplified but functional game

--- wumpus.rb
+++ wumpus.rb
@@ -16,16 +16,39 @@ loop do
     puts "The wumpus gobbled you up. GAME OVER!"
     exit
   else
+    if (current_room - wumpus_room).abs == 1
+      puts "You smell something terrible."
+    end
+
     puts "You are in room #{current_room}."
     puts "Exits go to #{current_room - 1} and #{current_room + 1}."
-    print "Where do you want to go? "
 
-    choice = gets.to_i
+    puts "What do you want to do? (m)ove or (s)hoot?"
+    action = gets.chomp
 
-    if (current_room - choice).abs == 1
-      current_room = choice
-    else
+    unless ["m","s"].include?(action)
+      puts "INVALID ACTION! TRY AGAIN!"
+      next
+    end
+
+    print "Where? "
+    room = gets.to_i
+
+    unless (current_room - room).abs == 1
       puts "THERE IS NO PATH TO THAT ROOM! TRY AGAIN!"
+      next
+    end
+
+    case action
+    when "m"
+      current_room = room
+    when "s"
+      if room == wumpus_room
+        puts "YOU KILLED THE WUMPUS! GOOD JOB, BUDDY!!!"
+        exit
+      else
+        puts "Your arrow didn't hit anything. Try a different room?"
+      end
     end
   end
 end
