9d42b878949d6bba57a9bec63a79d7fdad665a69
Gregory Brown <gregory.t.brown@gmail.com>
2013-11-21 12:30:49 -0500

    Add a callback based event system

--- wumpus.rb
+++ wumpus.rb
@@ -1,16 +1,25 @@
-#
-#    1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - 10
-# 
-
 require "set"
 
 class Room
   def initialize(number)
     @number    = number
     @neighbors = Set.new
+    @contents  = []
   end
 
   attr_reader :number, :neighbors
+
+  def add(thing)
+    @contents.push(thing)
+  end
+
+  def remove(thing)
+    @contents.delete(thing)
+  end
+
+  def has?(thing)
+    @contents.include?(thing)
+  end
 
   def connect(other_room)
     neighbors << other_room
@@ -18,66 +27,56 @@ class Room
     other_room.neighbors << self
   end
 
-  def neighboring_room_numbers 
+  def exits
     neighbors.map { |e| e.number }
   end
 
-  def find_neighbor(number)
+  def neighbor(number)
     neighbors.find { |e| e.number == number }
   end
 end
 
 class Narrator
-  def initialize(cave)
-    @cave         = cave
-    @current_room = cave.starting_room
+  def initialize(cave, player)
+    @cave   = cave
+    @player = player
+  end
+
+  def say(message)
+    puts message
+  end
+
+  def ask(question)
+    print "#{question} "
+    gets.chomp
+  end
+
+  def current_room
+    @cave.current_room
   end
 
   def describe_room
-    puts "-----------------------------------------"
-    puts "You are in room #{@current_room.number}."
+    say "-----------------------------------------"
+    say "You are in room #{current_room.number}."
 
-    if @current_room.neighbors.any? { |e| @cave.wumpus_room?(e) }
-      puts "You smell something terrible."
-    end
+    @player.investigate(current_room) 
 
-    if @current_room.neighbors.any? { |e| @cave.pit?(e) }
-      puts "You feel a cold wind flowing through the cavern"
-    end
-
-    if @current_room.neighbors.any? { |e| @cave.bat?(e) }
-      puts "You hear the rustling of bat wings nearby"
-    end
-
-    puts "Exits go to: #{exits.join(', ')}"
+    say "Exits go to: #{current_room.exits.join(', ')}"
   end
 
   def ask_player_to_act
     accepting_player_input do |action, dest|
       case action
       when "m"
-        @current_room = @current_room.find_neighbor(dest)
+        new_room = current_room.neighbor(dest)
 
-        case
-        when @cave.wumpus_room?(@current_room)
-          game_over("The wumpus gobbled you up. GAME OVER!")
-        when @cave.pit?(@current_room)
-          game_over("You stumbled into a bottomless pit. Have a nice fall!")
-        when @cave.bat?(@current_room)
-          puts "Giant bats whisk you away to a new cavern!"
-
-          new_room = @cave.random_room
-
-          @cave.bats.delete(@current_room) 
-          @cave.bats.push(new_room)
-
-          @current_room = new_room
-        end
+        @cave.move_to(new_room) # FIXME: awkward double call here
+        @player.enter(new_room) # .........
       when "s"
-        if @cave.wumpus_room?(@current_room.find_neighbor(dest))
-          game_over("YOU KILLED THE WUMPUS! GOOD JOB, BUDDY!!!")
+        if current_room.neighbor(dest).has?(:wumpus)
+          finish_story("YOU KILLED THE WUMPUS! GOOD JOB, BUDDY!!!")
         else
-          game_over("YOU SHOT INTO AN EMPTY ROOM. THIS WOKE THE WUMPUS "+
+          finish_story("YOU SHOT INTO AN EMPTY ROOM. THIS WOKE THE WUMPUS "+
                     "FROM HIS SLUMBER, AND HE GOBBLED YOU UP!")
         end
       end
@@ -89,35 +88,29 @@ class Narrator
   end
 
   def describe_ending
-    puts "-----------------------------------------"
-    puts @ending_message
+    say "-----------------------------------------"
+    say @ending_message
+  end
+
+  def finish_story(message)
+    @ending_message = message
   end
 
   private
 
-  def game_over(message)
-    @ending_message = message
-  end
-
-  def exits
-    @current_room.neighboring_room_numbers
-  end
-
   def accepting_player_input
-    puts "-----------------------------------------"
-    print "What do you want to do? (m)ove or (s)hoot? "
-    action = gets.chomp
+    say "-----------------------------------------"
+    action = ask("What do you want to do? (m)ove or (s)hoot?")
 
     unless ["m","s"].include?(action)
-      puts "INVALID ACTION! TRY AGAIN!"
+      say "INVALID ACTION! TRY AGAIN!"
       return
     end
 
-    print "Where? "
-    dest = gets.to_i
+    dest = ask("Where?").to_i
 
-    unless exits.include?(dest)
-      puts "THERE IS NO PATH TO THAT ROOM! TRY AGAIN!"
+    unless current_room.exits.include?(dest)
+      say "THERE IS NO PATH TO THAT ROOM! TRY AGAIN!"
       return
     end
 
@@ -127,8 +120,35 @@ end
 
 class Cave
   def initialize
-    @rooms = (1..20).map.with_object({}) { |i, h| h[i] = Room.new(i) }
+    @rooms        = (1..20).map.with_object({}) { |i, h| h[i] = Room.new(i) }
+    @current_room = random_room
 
+    build_dodechadron_layout
+
+    random_room.add(:wumpus)
+
+    3.times { random_room.add(:pit) }
+    3.times { random_room.add(:bats) }
+  end
+
+  attr_reader :current_room
+
+  def random_room
+    @rooms.values.sample
+  end
+
+  def move_to(room)
+    @current_room = room
+  end
+
+  def move(thing, from: raise, to: raise)
+    from.remove(thing)
+    to.add(thing)
+  end
+
+  private
+
+  def build_dodechadron_layout
     connections = [[1,2],[2,10],[10,11],[11,8],[8,1],
                    [1,5],[2,3],[9,10],[20,11],[7,8],
                    [5,4],[4,3],[3,12],[12,9],[9,19],
@@ -138,41 +158,83 @@ class Cave
                    [16,15],[15,14]]
 
     connections.each { |a,b| @rooms[a].connect(@rooms[b]) }
-
-    @starting_room = random_room
-    @wumpus_room   = random_room
-
-    @pits = 3.times.map { random_room }
-    @bats = 3.times.map { random_room }
   end
-
-  attr_reader :bats
-
-  def random_room
-    @rooms[rand(1..20)]
-  end
-
-  def wumpus_room?(room)
-    room.number == @wumpus_room.number
-  end
-
-  def pit?(room)
-    @pits.any? { |e| e.number == room.number }
-  end
-
-  def bat?(room)
-    @bats.any? { |e| e.number == room.number }
-  end
-
-  attr_reader :rooms, :starting_room, :wumpus_room
 end
 
-narrator = Narrator.new(Cave.new)
+class Player
+  def initialize
+    @senses     = {}
+    @encounters = {}
+  end
+
+  def sense(thing, &callback)
+    @senses[thing] = callback
+  end
+
+  def encounter(thing, &callback)
+    @encounters[thing] = callback
+  end
+
+  def enter(room)
+    @encounters.each do |thing, action|
+      return(action.call) if room.has?(thing)
+    end
+  end
+
+  def investigate(room)
+    @senses.each do |thing, action|
+      if room.neighbors.any? { |room| room.has?(thing) }
+        action.call
+      end
+    end
+  end
+end
+
+cave     = Cave.new
+player   = Player.new
+narrator = Narrator.new(cave, player)
+
+# Senses 
+
+player.sense(:bats) do
+  narrator.say("You hear a rustling sound nearby") 
+end
+
+player.sense(:wumpus) do
+  narrator.say("You smell something terrible nearby")
+end
+
+player.sense(:pit) do
+  narrator.say("You feel a cold wind blowing from a nearby cavern.")
+end
+
+# Encounters
+
+player.encounter(:bats) do
+  narrator.say "Giant bats whisk you away to a new cavern!"
+
+  new_room = cave.random_room
+
+  cave.move(:bats, from: cave.current_room, to: new_room)
+  cave.move_to(new_room)
+end
+
+player.encounter(:wumpus) do
+  narrator.finish_story("The wumpus ate you up!")
+end
+
+player.encounter(:pit) do
+  narrator.finish_story("You fell into a bottomless pit. Enjoy the ride!")
+end
+
+# Actions 
 
 until narrator.finished?
   narrator.describe_room
   narrator.ask_player_to_act
 end
+
+# Completion
 
 narrator.describe_ending
 
