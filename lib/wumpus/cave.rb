c65ebf5c8184074726c04760d98df95766609712
Gregory Brown <gregory.t.brown@gmail.com>
2013-12-09 20:14:40 -0500

    Add cave tests and class skeleton

--- lib/wumpus/cave.rb
+++ lib/wumpus/cave.rb
@@ -1,6 +1,41 @@
-warn "The Wumpus::Cave class has not yet been implemented"
+require "json"
 
 module Wumpus
   class Cave
+    def self.dodecahedron
+      raise NotImplementedError, "See lib/wumpus/cave.rb"
+    end
+
+    def self.from_json(filename)
+      raise NotImplementedError, "See lib/wumpus/cave.rb"
+    end
+
+    def initialize(edges)
+      raise NotImplementedError, "See lib/wumpus/cave.rb"
+    end
+
+    def add_hazard(thing, count)
+      raise NotImplementedError, "See lib/wumpus/cave.rb"
+    end
+
+    def random_room
+      raise NotImplementedError, "See lib/wumpus/cave.rb"
+    end
+
+    def move(thing, from: raise, to: raise)
+      raise NotImplementedError, "See lib/wumpus/cave.rb"
+    end
+
+    def room_with(thing)
+      raise NotImplementedError, "See lib/wumpus/cave.rb"
+    end
+
+    def entrance
+      raise NotImplementedError, "See lib/wumpus/cave.rb"
+    end
+
+    def room(number)
+      raise NotImplementedError, "See lib/wumpus/cave.rb"
+    end
   end
 end
