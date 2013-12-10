6c386e158eb550d896bf929d80340eff288c197d
Gregory Brown <gregory.t.brown@gmail.com>
2013-12-10 00:04:05 -0500

    Remove everything from master branch

--- README.md
+++
@@ -1,2 +0,0 @@
-A simplified implementation of Gregory Yob's "Hunt the Wumpus",
-written as an example for Practicing Ruby.
--- test/suite.rb
+++
@@ -1,8 +0,0 @@
-require "simplecov"
-
-SimpleCov.start { add_filter("/test/") }
-
-require_relative "unit/room_test"
-require_relative "unit/cave_test"
-require_relative "unit/player_test"
-require_relative "unit/narrator_test"
