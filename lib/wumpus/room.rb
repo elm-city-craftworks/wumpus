4d7bd13cbca02a5436e6c1fb38d8fe7769cd6c39
Gregory Brown <gregory.t.brown@gmail.com>
2013-12-09 20:09:43 -0500

    Add room tests and class skeleton

--- lib/wumpus/room.rb
+++ lib/wumpus/room.rb
@@ -1,6 +1,45 @@
-warn "The Wumpus::Room class has not yet been implemented"
-
 module Wumpus
   class Room
+    def initialize(number)
+      raise NotImplementedError, "See lib/wumpus/room.rb"
+    end
+
+    attr_reader :number, :neighbors
+
+    def add(thing)
+      raise NotImplementedError, "See lib/wumpus/room.rb"
+    end
+
+    def remove(thing)
+      raise NotImplementedError, "See lib/wumpus/room.rb"
+    end
+
+    def has?(thing)
+      raise NotImplementedError, "See lib/wumpus/room.rb"
+    end
+
+    def empty?
+      raise NotImplementedError, "See lib/wumpus/room.rb"
+    end
+
+    def safe?
+      raise NotImplementedError, "See lib/wumpus/room.rb"
+    end
+
+    def connect(other_room)
+      raise NotImplementedError, "See lib/wumpus/room.rb"
+    end
+
+    def exits
+      raise NotImplementedError, "See lib/wumpus/room.rb"
+    end
+
+    def neighbor(number)
+      raise NotImplementedError, "See lib/wumpus/room.rb"
+    end
+
+    def random_neighbor
+      raise NotImplementedError, "See lib/wumpus/room.rb"
+    end
   end
 end
