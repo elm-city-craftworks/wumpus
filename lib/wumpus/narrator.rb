9e728d454fc86c4341661d9876f10ad79545460c
Gregory Brown <gregory.t.brown@gmail.com>
2013-12-10 10:54:40 -0500

    Add narrator to game executable branch

--- lib/wumpus/narrator.rb
+++ lib/wumpus/narrator.rb
@@ -1,6 +1,31 @@
-warn "The Wumpus::Narrator class has not yet been implemented"
-
 module Wumpus
   class Narrator
+    def say(message)
+      $stdout.puts message
+    end
+
+    def ask(question)
+      print "#{question} "
+      $stdin.gets.chomp
+    end
+
+    def tell_story
+      yield until finished?
+  
+      say "-----------------------------------------"
+      describe_ending
+    end
+
+    def finish_story(message)
+      @ending_message = message
+    end
+ 
+    def finished?
+      !!@ending_message
+    end
+
+    def describe_ending
+      say @ending_message
+    end
   end
 end
